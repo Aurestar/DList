8bace17d4694125e2fe987b93330188706aaf3ea
Aurélien Mazé <aurestar200526@gmail.com>
2022-06-21 15:26:46 +0200

    Update

--- background.js
+++ background.js
@@ -1,12 +1,6 @@
-
-
-
-
 	chrome.browserAction.onClicked.addListener(function(tab) {
 	  
 	  chrome.tabs.executeScript({
-		code: 'var src=document.getElementsByTagName("audio"); var load=src[1].src; location.href = load;'
+		code: 'var src=document.getElementsByTagName("audio");var load=src[1].src;var title=document.querySelector(".footer-song-name").innerHTML;var author=document.querySelector(".footer-artist-name").innerHTML;author.substr(4, 2000);var fileName="";fileName.concat("_",author, "-", title, ".aac");'
 	  });
-	  
-	  
 	});
