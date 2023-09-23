3bf498c8f3a60fb3cecfdddb28e10eaaef2a571a
Aurélien Mazé <93390505+Aurestar@users.noreply.github.com>
2023-09-24 01:01:19 +0200

    1.3 update

--- README.md
+++ README.md
@@ -13,10 +13,11 @@ Installation
 * Click on the "Load unpackaged extension" button
 * Select the folder containing the extension
 * Your extension is ready
+* (optional) Pin the extension
 
 Usage
 ----------------------------
-* Go to the Artlist.io website
+* Go on Artlist.io, MotionArray.com or UpBeat.io
 * Find the music that suits you
 * Start the music (important)
 * Click on the exension logo on the top left of your browser
--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
 	
 	chrome.tabs.executeScript({
-	code: 'var src=document.getElementsByTagName("audio");var fileUrl=src[1].src;var title=document.querySelector(".footer-song-name").innerHTML;var author=document.querySelector(".footer-artist-name").innerHTML;author=author.substr(4, 2000);var fileName="";fileName=fileName.concat("_",author, "-", title, ".aac");var element = document.createElement("a");element.setAttribute("href","data:text/plain;charset=utf-8, " + encodeURIComponent(fileUrl));element.setAttribute("download", fileName);document.body.appendChild(element);element.click();'
+		code: 'const audioTag = document.querySelector("audio"); const audioSrc = audioTag.src;window.open(audioSrc);'
 	});
 });
-	
